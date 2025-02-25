3a360998a8bfc5e74db1b0b64d7141b91ba9d636
MBinMohsin <mbinmohsin.5426@gmail.com>
2025-02-25 17:42:32 +0500

    Create UserInfo.java

---
+++ Class 3/UserInfo.java
@@ -0,0 +1,12 @@
+public class UserInfo{
+	
+	private static String userName ="Admin";
+	private static String password = "Admin123";
+
+	public String getUserN(){
+		return userName;
+	}
+	public String getUserP(){
+		return password;
+	} 
+}
