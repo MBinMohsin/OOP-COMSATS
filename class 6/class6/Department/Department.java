702a51dc9e4e0491e849562a3472633b9e185e89
MBinMohsin <mbinmohsin.5426@gmail.com>
2025-03-18 14:23:28 +0500

    Create Department.java

---
+++ class 6/class6/Department/Department.java
@@ -0,0 +1,20 @@
+package class6.Department;
+
+import class6.Employee.*;
+import class6.Lab.*;
+
+public class Department{
+
+	 Employee human;
+	 String name;
+	 String labIncharge;
+	Lab labs[];
+
+	public Department(String name, Employee human, String labIncharge, Lab labs[]){
+		this.name = name;
+		this.hod = hod;
+		this.labIncharge = labIncharge;
+		this.labs = labs;
+	}
+
+}
